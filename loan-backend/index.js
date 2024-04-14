b0cd5e158cce507e6bab96ed2a1504e70d620216
Ricardo <ricardo.lopez@taxtris.com>
2024-04-13 21:52:47 -0600

    Adding zero knowledge proof basic concept

--- loan-backend/index.js
+++ loan-backend/index.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(cors());
 app.use(express.json()); // middleware to parse JSON request bodies
 
-app.post('/generate-loan-proof', async (req, res) => {
+app.post('/verify-transaction', async (req, res) => {
   const { loanAmount } = req.body;
 
   // Assume we have a predefined ZKP circuit and keys already set up
@@ -17,7 +17,7 @@ app.post('/generate-loan-proof', async (req, res) => {
   };
 
   try {
-    const { proof, publicSignals } = await snarkjs.groth16.fullProve(input, "Balancecheck.wasm", "circuit_final.zkey");
+    const { proof, publicSignals } = await snarkjs.groth16.fullProve(input, "Balancecheck.wasm", "BalanceCheck.zkey");
 
     console.log("Proof: ", proof);
     console.log("Public signals: ", publicSignals);
